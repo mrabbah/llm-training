c9bf6aa8cdb3726ccbd651919cabad36a66c20a9
RABBAH Mahmoud <mrabbah@gmail.com>
2023-12-31 18:05:53 +0100

    Update stablecode-completion-alpha-3b-test.py and mta.py

--- model-training-anatomy/mta.py
+++ model-training-anatomy/mta.py
@@ -1,5 +1,6 @@
 import numpy as np
 from datasets import Dataset
+from pynvml import *
 
 
 seq_len, dataset_size = 512, 512
@@ -9,3 +10,17 @@ dummy_data = {
 }
 ds = Dataset.from_dict(dummy_data)
 ds.set_format("pt")
+
+def print_gpu_utilization():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    print(f"GPU memory occupied: {info.used//1024**2} MB.")
+
+
+def print_summary(result):
+    print(f"Time: {result.metrics['train_runtime']:.2f}")
+    print(f"Samples/second: {result.metrics['train_samples_per_second']:.2f}")
+    print_gpu_utilization()
+
+print_gpu_utilization()
