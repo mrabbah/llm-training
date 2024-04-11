8c672fe1a0f2269c98544af42f7dde085d847c41
RABBAH Mahmoud <mrabbah@gmail.com>
2024-04-11 13:05:33 +0000

    Refactor mta.py for model training and dataset loading

--- model-training-anatomy/mta.py
+++ model-training-anatomy/mta.py
@@ -1,17 +1,11 @@
 import numpy as np
-from datasets import Dataset
+from datasets import load_dataset
 from pynvml import *
 from transformers import TrainingArguments, Trainer, logging, AutoModelForSequenceClassification, AutoTokenizer
 import torch
+from sklearn.preprocessing import LabelEncoder
 
-seq_len, dataset_size = 512, 512
-dummy_data = {
-    "input_ids": np.random.randint(100, 30000, (dataset_size, seq_len)),
-    "labels": np.random.randint(0, 1, (dataset_size)),
-}
-ds = Dataset.from_dict(dummy_data)
-ds.set_format("pt")
-
+# utilities function for monitoring progress:
 def print_gpu_utilization():
     nvmlInit()
     handle = nvmlDeviceGetHandleByIndex(0)
@@ -26,27 +20,26 @@ def print_summary(result):
 
 print_gpu_utilization()
 
-torch.ones((1, 1)).to("cuda")
-print_gpu_utilization()
+# loading dataset
+ds = load_dataset("papluca/language-identification")
+ds.set_format("pt")
+print(ds)
+print(ds["train"][0])
 
-MODEL_ID = "TheBloke/OpenHermes-2.5-Mistral-7B-GPTQ"
-# MODEL_ID = "Felladrin/TinyMistral-248M-SFT-v4"
-# model = AutoModelForSequenceClassification.from_pretrained(
-#     MODEL_ID, 
-#     torch_dtype=torch.float16,
-#     use_flash_attention_2=True).to("cuda")
-device = "cuda" # if torch.cuda.is_available() else "cpu"
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_ID, device_map=device) 
-# model = AutoModelForSequenceClassification.from_pretrained(MODEL_ID).to("cuda")
-print_gpu_utilization()
 
-default_args = {
-    "output_dir": "tmp",
-    "evaluation_strategy": "steps",
-    "num_train_epochs": 1,
-    "log_level": "error",
-    "report_to": "none",
-}
+# labels are in string format example "en", "fr", "de", "ar"... we must convert them to integers
+# before feeding them to the model
+label_encoder = LabelEncoder()
+label_encoder.fit(ds["train"]["labels"])
+print(label_encoder.classes_)
+print(label_encoder.transform(label_encoder.classes_))
+
+# loading the model & the tokenizer
+MODEL_ID = "Felladrin/TinyMistral-248M-SFT-v4"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_ID, device_map=device, num_labels=len(label_encoder.classes_),) 
+
+print_gpu_utilization()
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
 tokenizer.pad_token = tokenizer.eos_token
@@ -54,10 +47,49 @@ tokenizer.padding_side = "right"
 model.config.pad_token_id = model.config.eos_token_id
 
 logging.set_verbosity_error()
-max_seq_length = 2048
 
-training_args = TrainingArguments(per_device_train_batch_size=4, optim="adafactor", gradient_checkpointing=True, fp16=True, **default_args)
-trainer = Trainer(model=model, args=training_args, train_dataset=ds, tokenizer=tokenizer)
+# Tokenizing the dataset
+def tokenize(batch):
+    tokenized_inputs = tokenizer(batch["text"], padding=True, truncation=True)
+    tokenized_inputs["labels"] = label_encoder.transform(batch["labels"])
+    return tokenized_inputs
+
+train_dataset=ds["train"].map(tokenize, batched=True)
+eval_dataset=ds["validation"].map(tokenize, batched=True)
+test_dataset=ds["test"].map(tokenize, batched=True)
+
+# Setting up the training arguments
+default_args = {
+    "output_dir": "./tmp",
+    "logging_dir": './logs',
+    "evaluation_strategy": "steps",
+    "num_train_epochs": 1,
+    "log_level": "error",
+    "report_to": "none",
+}
+
+training_args = TrainingArguments(
+    per_device_train_batch_size=4, 
+    optim="adafactor", 
+    gradient_checkpointing=True, 
+    fp16=True, 
+    **default_args
+)
+
+# Training the model
+trainer = Trainer(
+    model=model, 
+    args=training_args, 
+    train_dataset=train_dataset, 
+    eval_dataset=eval_dataset,
+    tokenizer=tokenizer
+)
 result = trainer.train()
 print_summary(result)
+
+# Perform evaluation on the test dataset
+eval_result = trainer.evaluate()
+print(f"Evaluation result: {eval_result}")
+
+
 
